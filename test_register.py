f37b947b13b0b029eee289feb7d0bba458d6ed9d
Atta Ur Rehman <ATTIQURESHI56@gmail.com>
2026-01-06 21:08:38 +0500

    Delete test_register.py

--- test_register.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:8000/auth/register"
-payload = {"email": "test@example.com", "password": "test123"}
-response = requests.post(url, json=payload)
-print("Status:", response.status_code)
-print("Response:", response.text)
